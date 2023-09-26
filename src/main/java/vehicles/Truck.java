48230c91c0d5ef4bc257da88e0754700197feb61
marishkazachariah <marishka.zachariah@gmail.com>
2023-09-26 16:12:09 +0200

    change second line to Truck class

--- src/main/java/vehicles/Truck.java
+++ src/main/java/vehicles/Truck.java
@@ -3,6 +3,5 @@ package vehicles;
 public class Truck {
     public static void main(String[] args) {
         System.out.println("This is a truck.");
-        System.out.println("Here is another line");
     }
 }
